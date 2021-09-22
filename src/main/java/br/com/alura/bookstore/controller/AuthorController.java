66fe113a50d2578f6e2168023d750bb330277c22
Gislaine Mendes <gislainemrocha@gmail.com>
2021-09-22 19:56:25 -0300

    Adding controller package

---
+++ src/main/java/br/com/alura/bookstore/controller/AuthorController.java
@@ -0,0 +1,28 @@
+package br.com.alura.bookstore.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.bookstore.model.Author;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+	
+	private List<Author> authors = new ArrayList<>();
+	
+	@GetMapping
+	public List<Author> toList() {
+		return authors;
+	}
+	
+	@PostMapping
+	public void toSave(Author author) {
+		authors.add(author);
+	}
+}
