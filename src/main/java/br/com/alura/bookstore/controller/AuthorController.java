7bf2f1fe6a026e4c2eda50e65f71d8cf83f753bd
Gislaine Mendes <gislainemrocha@gmail.com>
2021-09-23 18:44:47 -0300

    Refactoring PostMapping method

--- src/main/java/br/com/alura/bookstore/controller/AuthorController.java
+++ src/main/java/br/com/alura/bookstore/controller/AuthorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class AuthorController {
 	}
 	
 	@PostMapping
-	public void toSave(Author author) {
+	public void toSave(@RequestBody Author author) {
 		authors.add(author);
 	}
 }
