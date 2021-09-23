bd11de326d6cd33e96cdee79c213f7b7f9bd13ec
Gislaine Mendes <gislainemrocha@gmail.com>
2021-09-23 18:45:16 -0300

    Adding AuthorsDto class

---
+++ src/main/java/br/com/alura/bookstore/dto/AuthorsDto.java
@@ -0,0 +1,35 @@
+package br.com.alura.bookstore.dto;
+
+import java.time.LocalDate;
+
+public class AuthorsDto {
+
+	private String name;
+	private String email;
+	private LocalDate birthDate;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+}
