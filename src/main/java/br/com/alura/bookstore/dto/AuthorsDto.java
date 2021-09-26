12381b6c5f3c3bb8db0f1943df2a65584e50d5cc
Gislaine Mendes <gislainemrocha@gmail.com>
2021-09-26 11:53:03 -0300

    Adding Lombok config

--- src/main/java/br/com/alura/bookstore/dto/AuthorsDto.java
+++ src/main/java/br/com/alura/bookstore/dto/AuthorsDto.java
@@ -1,35 +1,17 @@
 package br.com.alura.bookstore.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class AuthorsDto {
 
-	private String name;
-	private String email;
-	private LocalDate birthDate;
+    private String name;
+    private String email;
+    private LocalDate birthDate;
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public LocalDate getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(LocalDate birthDate) {
-		this.birthDate = birthDate;
-	}
 
 }
--- src/main/java/br/com/alura/bookstore/dto/AuthorsFormDto.java
+++ src/main/java/br/com/alura/bookstore/dto/AuthorsFormDto.java
@@ -1,44 +1,18 @@
 package br.com.alura.bookstore.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class AuthorsFormDto {
 
-	private String name;
-	private String email;
-	private LocalDate birthDate;
-	private String curriculo;
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+    private String curriculo;
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public LocalDate getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(LocalDate birthDate) {
-		this.birthDate = birthDate;
-	}
-
-	public String getCurriculo() {
-		return curriculo;
-	}
-
-	public void setCurriculo(String curriculo) {
-		this.curriculo = curriculo;
-	}
 
 }
