443834f731670d8da07c1771badb068cebfe173f
Gislaine Mendes <gislainemrocha@gmail.com>
2021-10-08 20:37:08 -0300

    Refactoring service classes

--- src/main/java/br/com/alura/bookstore/service/AuthorService.java
+++ src/main/java/br/com/alura/bookstore/service/AuthorService.java
@@ -1,9 +1,13 @@
 package br.com.alura.bookstore.service;
 
+import br.com.alura.bookstore.dao.AuthorRepository;
+import br.com.alura.bookstore.dao.BookRepository;
 import br.com.alura.bookstore.dto.AuthorsDto;
 import br.com.alura.bookstore.dto.AuthorsFormDto;
 import br.com.alura.bookstore.model.Author;
+import br.com.alura.bookstore.model.Book;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,19 +19,22 @@ import java.util.stream.Collectors;
 @Service
 public class AuthorService {
 
-    private List<Author> authors = new ArrayList<>();
+    @Autowired
+    private AuthorRepository authorRepository;
     private ModelMapper modelMapper = new ModelMapper();
 
     public List<AuthorsDto> toList() {
+        List<Author> authors = authorRepository.findAll();
         return authors.stream().map(a -> modelMapper.map(a, AuthorsDto.class)).collect(Collectors.toList());
     }
 
     public void toSave(@RequestBody @Valid AuthorsFormDto dto) {
         Author author = modelMapper.map(dto, Author.class);
-        authors.add(author);
+        authorRepository.save(author);
     }
 
     public Author findAuthorByName(String name){
+        List<Author> authors = authorRepository.findAll();
         return authors.stream().filter(author -> author
                 .getName()
                 .equals(name))
--- src/main/java/br/com/alura/bookstore/service/BookService.java
+++ src/main/java/br/com/alura/bookstore/service/BookService.java
@@ -1,5 +1,6 @@
 package br.com.alura.bookstore.service;
 
+import br.com.alura.bookstore.dao.BookRepository;
 import br.com.alura.bookstore.dto.BooksDto;
 import br.com.alura.bookstore.dto.BooksFormDto;
 import br.com.alura.bookstore.model.Author;
@@ -7,30 +8,29 @@ import br.com.alura.bookstore.model.Book;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class BookService {
 
-    private List<Book> books = new ArrayList<>();
+    @Autowired
+    private BookRepository bookRepository;
     private ModelMapper modelMapper = new ModelMapper();
 
     @Autowired
     private AuthorService authorService;
 
     public List<BooksDto> toList() {
+        List<Book> books = bookRepository.findAll();
         return books.stream().map(b -> modelMapper.map(b, BooksDto.class)).collect(Collectors.toList());
     }
 
-    public void toSave(@RequestBody @Valid BooksFormDto dto) {
+    public void toSave(BooksFormDto dto) {
         Author author = authorService.findAuthorByName(dto.getAuthor());
         Book book = modelMapper.map(dto, Book.class);
         book.setAuthor(author);
-        books.add(book);
+        bookRepository.save(book);
     }
 }
